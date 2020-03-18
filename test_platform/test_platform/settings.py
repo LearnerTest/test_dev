3b12275a55a718f5a04cf7b7b47a1359b93d8599
lij3 <lij3@epsoft.com.cn>
2020-03-18 15:12:45 +0800

    0318更新settings

--- test_platform/test_platform/settings.py
+++ test_platform/test_platform/settings.py
@@ -40,13 +40,13 @@ INSTALLED_APPS = [
     'person',#需要添加应用
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
